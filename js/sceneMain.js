d7dea34fab4fef14f096378840c7674528379992
markallen <markallen.webdeveloper@gmail.com>
2019-03-27 14:09:38 +0000

    First commit of toolbox

---
+++ js/sceneMain.js
@@ -0,0 +1,24 @@
+class SceneMain extends Phaser.Scene {
+  constructor() {
+      super('SceneMain');
+  }
+  preload()
+  {
+    // Preload resources (e.g images, sounds, etc...)
+    // this.load.image("key", "path");
+
+  }
+  create() {
+    // Define our objects (e.g. bullets, spaceships, etc...)
+    // this.key = this.add.image(x, y, "key");
+
+  }
+  update() {
+    // Constantly running loop (e.g. to check for collisions or for things that need to be constantly updated)
+
+  }
+  customFunction() {
+    // Any additional functionality we require can be added with custom functions
+    
+  }
+}
